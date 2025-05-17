a9ff83d2bcdbec687e9021c07b4707acce06d59b
Jack O'Donnell <jacksonhodonnell@gmail.com>
2025-05-17 08:50:18 -0700

    better package exports

--- sidm_halos/__init__.py
+++ sidm_halos/__init__.py
@@ -1,3 +1,6 @@
 from .jeans_halo import (SIDMHaloSolution, OuterNFW, InnerIsothermal)
 from .jeans_solvers.error import SIDMSolutionError
 from .cse.cse_decomp import CSERepr
+from .baryon_profiles import BaryonProfile, DPIEProfile
+from .util import require_units
+from . import cosmology
